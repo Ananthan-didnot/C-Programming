4f426ee6a7912e273ce25b53b09d673916280421
Ananthakrishnan K V <ananthakrishnanvenu007@gmail.com>
2025-04-09 16:22:31 +0530

    Create readFiles.c

---
+++ readFiles.c
@@ -0,0 +1,32 @@
+/*Write a C program to read a file and count number of words,lines and characters in it*/
+
+#include <stdio.h>
+int main()
+{
+    FILE *fp;
+    int words = 0, lines = 0, character = 0;
+    fp = fopen("text.txt", "r");
+    char ch;
+    while (fscanf(fp, "%c", &ch) == 1)
+    {
+        character++;
+
+        if (ch == '\n')
+        {
+            lines++;
+            words++;
+        }
+
+        if (ch == ' ' || ch == '.')
+        {
+            words++;
+        }
+    }
+    if (character > 1)
+    {
+        lines += 1;
+    }
+    fclose(fp);
+    printf("Words = %d\nLines = %d\nCharacters = %d\n", words, lines, character);
+    return 0;
+}
